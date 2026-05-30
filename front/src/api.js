3b5bb4dc1a12d14d5af898b7265caaee8873b241
aissatabarry2 <ayebarry28@gmail.com>
2026-05-30 11:10:11 +0000

    Update API_URL to Render backend

--- front/src/api.js
+++ front/src/api.js
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:5000";
+//const API_URL = "http://localhost:5000";
+const API_URL = "https://eva-composant-logiciel-back.vercel.app";
 
 export async function getMessage() {
   const res = await fetch(API_URL + "/");
